d4ccfaa4d3e03aee9ce3de7650b44391272bc18c
github username <github email address>
2020-06-23 15:51:58 -0400

    Complete assessment with strong params and model validations

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class KidsController < ApplicationController
   def index
     @kids = Kid.all
@@ -6,13 +8,18 @@ class KidsController < ApplicationController
   end
 
   def create
-    @kid = Kid.create({
-      username: params[:kid][:username],
-      description: params[:kid][:description],
-      favorite_color: params[:kid][:favorite_color],
-      age: params[:kid][:age],
-    })
+    @kid = Kid.create kid_params
 
-    render json: { kid: @kid }
+    if @kid.errors.any?
+      render @kid.errors.full_messages
+    else
+      render json: { kid: @kid }
+    end
+  end
+
+  private
+
+  def kid_params
+    params.require(:kid).permit(:username, :description, :favorite_color, :age)
   end
 end
--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Kid < ApplicationRecord
+  validates :username, { presence: true, uniqueness: true, length: { in: 7..25 } }
+  validates :age, { presence: true, inclusion: { in: 6...15 } }
 end
